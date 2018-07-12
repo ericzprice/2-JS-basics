d01a1c393264c28dd3fa047029a8199e7a9ff8a1
ericzprice <ericzprice@gmail.com>
2018-07-12 11:09:25 -0600

    Variable mutation and type coercion

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 // console.log('Hello World!');
+// ****************************************
+// // Variables and data types
 //
-var firstName = 'John';
-console.log(firstName);
 
-var lastName = 'Smith';
+// //
+// var firstName = 'John';
+// console.log(firstName);
+//
+// var lastName = 'Smith';
+// var age = 28;
+//
+// var fullAge= true;
+// console.log(fullAge);
+//
+// var job;
+// console.log(job);
+//
+// job = 'Teacher';
+// console.log(job);
+//
+// // Var naming rules
+// // All var names must start with a $, _, or letter...and NOT JS names like var, if...
+// var _3years = 3;
+// var john/mark = 'John and Mark';
+// var if = 23;
+
+// ******************************************
+// Variable mutation and type coercion
+//*******************************************
+var firstName = 'John';
 var age = 28;
 
-var fullAge= true;
-console.log(fullAge);
+// Type coercion
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
 
-job = 'Teacher';
-console.log(job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-// All var names must start with a $, _, or letter...and not JS names like var, if...
-var _3years = 3;
-var john/mark = 'John and Mark';
+// Variable mutation
+// Changeing value of a var after var is already declared with a value above in the code
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
