d57100e328bfe5c780a22e82ae36813b434a0c12
ericzprice <ericzprice@gmail.com>
2018-07-12 10:39:11 -0600

    Variables and data types

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+// console.log('Hello World!');
+//
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge= true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// All var names must start with a $, _, or letter...and not JS names like var, if...
+var _3years = 3;
+var john/mark = 'John and Mark';
